f40cfe59396b1202ad1dbb45d1d51e658946bced
ValentinaPolishchuk <tinapolishchuk21@gmail.com>
2023-12-05 13:20:22 -0500

    My first automation

---
+++ src/test/java/com/loop/test/day1_selenium_intro/PracticeById.java
@@ -0,0 +1,21 @@
+package com.loop.test.day1_selenium_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PracticeById {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.navigate().to("https://google.com");
+        WebElement search = driver.findElement(By.id("APjFqb"));
+        search.sendKeys("Loop Academy");
+    }
+}
